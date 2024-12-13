1d93508b621a30082af8faa9bab64a35ba3f5ffe
ArmandleRouxx <armandlr@hyperiondev.com>
2024-12-13 18:11:24 +0200

    Added code examples from Q&A

---
+++ CyberSecurity (CS)/Week01/Q&A/examples.py
@@ -0,0 +1,12 @@
+name1 = "Peter"
+name2 = "David"
+name3 = "Tina"
+
+# print(name1 + "\n" + name2 + "\n" + name3)
+# print(f"{name1}\n{name2}\n{name3}")
+
+# print(name1, name2, name3, sep="\n")
+
+
+print("Hello world!")
+print("-"*80)
